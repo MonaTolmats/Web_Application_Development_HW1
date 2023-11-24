2622671048759f5d7898f1dca42594920aea130c
Meus <karl.andreas@meus.ee>
2023-11-24 16:12:07 +0200

    posts workin in vue

--- my_vue/src/components/Posts.vue
+++ my_vue/src/components/Posts.vue
@@ -1,28 +1,55 @@
 <template>
-    <div class = container>
-        <right-sidebar></right-sidebar>
-        <div class = postsContainer>
-            <h1>Posts will be here soon</h1>
-            
+  <div class="container">
+    <left-sidebar></left-sidebar>
+    <div id="postsContainer">
+      <div class="post" v-for="post in posts" :key="post.postId">
+        <div class="post_header">
+          <img class="post_logo" :src="'/icon.ico'" alt="Author's logo">
+          <p class="post_date">{{ post.createTime }}</p>
         </div>
-        <left-sidebar></left-sidebar>
+        <div>
+          <img v-if="post.image" class="post_image" :src="post.image" :alt="post.title">
+          <p class="post_text">{{ post.title }}</p>
+          <img class="post_logo" src="/like.png" alt="Like button">
+        </div>
+      </div>
     </div>
-    <footer>
-    </footer>
+    <right-sidebar></right-sidebar>
+  </div>
+  <footer-component></footer-component>
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
-import RightSidebar from '../components/RightSidebar.vue'
-import LeftSidebar from '../components/LeftSidebar.vue'
+import FooterComponent from '@/components/Footer.vue'
+import LeftSidebar from '@/components/LeftSidebar.vue'
+import RightSidebar from '@/components/RightSidebar.vue'
 
 export default {
   name: 'PostsContainer',
   components: {
-    Footer, RightSidebar, LeftSidebar
+    FooterComponent, LeftSidebar, RightSidebar
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    fetchPosts() {
+      fetch('/posts.json') // Replace with the actual path to your JSON file
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+        })
+        .catch(error => console.error('Error fetching posts:', error));
+    }
   }
 }
 </script>
+
 
 <style scoped>
 .container{
@@ -31,6 +58,19 @@ export default {
     top: 0%;
     padding-top: 9px;
     padding-bottom: 9px;
+
+}
+
+.post_logo, .like_button {
+  /* Adjust the size as needed */
+  width: 25px; 
+  height: 25px;
+}
+
+.post_image {
+  max-width: 50%; /* Ensures the image is not wider than its container */
+  height: auto; /* Maintains the aspect ratio of the image */
+  border-radius: 10px; /* Optional: for rounded corners */
 }
    
 
